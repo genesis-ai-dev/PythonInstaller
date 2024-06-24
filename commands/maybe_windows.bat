dcb823b2e8087aee847536a835581c9908d37271
dadukhankevin <danieljlosey@gmail.com>
2024-06-24 09:15:21 -0500

    better windows command`

--- commands/maybe_windows.bat
+++ commands/maybe_windows.bat
@@ -1,1 +1,10 @@
-python3
+@echo off
+python3 --version >nul 2>&1
+if %errorlevel% equ 0 (
+    if not exist venv (
+        python3 -m venv venv
+    )
+    venv\Scripts\activate.bat
+) else (
+    python3
+)
